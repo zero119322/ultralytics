dc6394fc4d7bf63610111f6db9eaebbf9a38b245
zero119322 <58361873+zero119322@users.noreply.github.com>
2024-08-01 19:13:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("ultralytics/cfg/models/v8/yolov8n.yaml")  # build a new model from scratch
+model = YOLO("yolov8n.pt")
 
 
 # Use the model
-model.train(data="VOC.yaml", epochs=10,device='0',pretrained=False,seed=42,weights=None)  # train the model
+model.train(data="VOC.yaml", epochs=10,device='0',pretrained=False,seed=42)  # train the model
 
