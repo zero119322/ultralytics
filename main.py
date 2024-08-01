514ee108e27af212ead163e8434b56728dfef5a2
zero119322 <58361873+zero119322@users.noreply.github.com>
2024-08-01 18:51:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("ultralytics/cfg/models/v8/yolov8.yaml")  # build a new model from scratch
+model = YOLO("ultralytics/cfg/models/v8/yolov8s.yaml")  # build a new model from scratch
 
 
 # Use the model
-model.train(data="coco128.yaml", epochs=500,device='0',pretrained=False)  # train the model
-metrics = model.val()  # evaluate model performance on the validation set
-results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
-path = model.export(format="onnx")  # export the model to ONNX format
+model.train(data="VOC.yaml", epochs=50,device='0',pretrained=False,seed=42)  # train the model
+
