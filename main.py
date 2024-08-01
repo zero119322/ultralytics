f2fb49c5ca24a24f1a0bfab534a5534189acf12f
zero119322 <58361873+zero119322@users.noreply.github.com>
2024-08-01 23:03:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8n.yaml")
+model = YOLO("yolov8n-pose.yaml")
 
 
 # Use the model
-model.train(data="VOC.yaml", epochs=10,device='0',pretrained=False,seed=42)  # train the model
+model.train(data="lsp.yaml", epochs=10,device='0',pretrained=False,seed=42)  # train the model
 
