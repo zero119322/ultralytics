902d1ee30d7d552db1cf9d9cee9fc76a05ae22d2
zero119322 <58361873+zero119322@users.noreply.github.com>
2024-08-05 00:32:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ model = YOLO("yolov8n-pose.pt")
 
 
 # Use the model
-model.train(data="lsp.yaml", epochs=50,device='0',pretrained=true,seed=42)  # train the model
+model.train(data="lsp.yaml", epochs=50,device='0',pretrained=True,seed=42)  # train the model
 
