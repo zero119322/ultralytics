b9cc34e0e52a7e520144ce39940fe235ddc71c3e
zero119322 <58361873+zero119322@users.noreply.github.com>
2024-08-06 16:50:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from ultralytics import YOLO
+import os
+
 if __name__ == '__main__':
     # 加载模型
-    model = YOLO("ultralytics/cfg/models/v8/yolov8n.yaml")  # 从头开始构建新模型
-    print(model.model)
+    model = YOLO(model="ultralytics/cfg/models/v8/yolov8l.yaml")  # 从头开始构建新模型
 
     # Use the model
-    results = model.train(data="VOC1.yaml", epochs=100, device='0', batch=16,workers=0)  # 训练模型
+    results = model.train(data="VOC.yaml", epochs=300, device='0', batch=8, seed=42,patience=10)  # 训练模
