b8cf2085658b4f82cc6f52eb9daa0a9ca63321e2
zero119322 <58361873+zero119322@users.noreply.github.com>
2024-08-08 00:54:35 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
+
 from ultralytics import YOLO
 
 
 if __name__ == '__main__':
     # Load a model
     # model = YOLO('yolov8m.pt')  # load an official model
-    model = YOLO('ultralytics/runs/detect/train/weights/best.pt')  # 使用相对路径
-    results = model.predict(source="ultralytics/assets", device='0',save=True)  # predict on an image
-    print(results)
+    model = YOLO('best.pt')  # load a custom model
+
+
+    # Validate the model
+    metrics = model.val(split='val')  # no arguments needed, dataset and settings remembered
+
+
